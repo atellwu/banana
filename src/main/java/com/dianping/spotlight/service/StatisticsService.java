e31a39aff15bdbcf9656b302dddf06faf9b73158
Leo Liang <liangjinhua@gmail.com>
2013-05-17 22:01:30 +0800

    add init

--- src/main/java/com/dianping/spotlight/service/StatisticsService.java
+++ src/main/java/com/dianping/spotlight/service/StatisticsService.java
@@ -3,7 +3,9 @@ package com.dianping.spotlight.service;
 import java.util.Set;
 
 public interface StatisticsService {
-	public Set<Hotkey> listHotkeys(String appName);
+    public void init();
 
-	public double record(Set<Hotkey> hotkeys, int score);
+    public Set<Hotkey> listHotkeys(String appName);
+
+    public double record(Set<Hotkey> hotkeys, int score);
 }
